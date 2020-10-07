070e207dab2a1a103e40acd5012bb83580f3c545
Harsh Rumalwala <harsh.ashvin@gmail.com>
2020-10-08 01:38:03 +0530

    integrating user login on UI and setting token in global context to be used across pages

--- src/client/pages/home/index.jsx
+++ src/client/pages/home/index.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import _ from "lodash";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import { useCurrentUser } from "client/hooks";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  login: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const classes = useStyles();
+  const history = useHistory();
+  const { token } = useCurrentUser();
+
+  const handleLogin = () => {
+    history.push("/login");
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.container}>
+        <Typography component="h1" variant="h5">
+          Home
+        </Typography>
+        {!_.isNil(token) ? (
+          <span>User with token - {token} is logged in</span>
+        ) : (
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.login}
+            onClick={handleLogin}
+          >
+            Log In
+          </Button>
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
--- src/client/routes/index.jsx
+++ src/client/routes/index.jsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
+import { Loader } from "client/components";
 
 const Home = lazy(() => import("client/pages/home"));
 const Login = lazy(() => import("client/pages/login"));
@@ -8,7 +9,7 @@ const Room = lazy(() => import("client/pages/room"));
 
 const Routes = () => (
   <Switch>
-    <Suspense fallback={<h1>Loading Page</h1>}>
+    <Suspense fallback={<Loader />}>
       <Route path="/room/:roomId" component={Room} />
       <Route path="/login" component={Login} />
       <Route path="/profile" component={Profile} />
